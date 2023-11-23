4c1618f7ed2516e459c1a9d7bebb54d704f4b5f7
lakshmi narayana <lakshminarayana>
2023-11-23 22:04:19 +0530

    First commit

---
+++ src/com/aja/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.aja.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="lucky";
+		System.out.println(s);
+	}
+
+}
